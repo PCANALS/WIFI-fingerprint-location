88b332236aee06dfc1a1218c287be31c1d518452
Pilar <pilarcanalsdros@gmail.com>
2018-12-03 18:22:28 +0100

    first subsets

---
+++ WIFI.R
@@ -0,0 +1,49 @@
+#### ._DEFAULT SCRIPT ####
+
+rm(list = ls())
+
+Sys.setlocale(category = "LC_ALL", locale = "english")
+
+
+#### ._LIBRARIES####
+library(dplyr)
+library(ggplot2)
+
+#### A._ SETTING FILES####
+
+setwd("C:/Users/pilar/Google Drive/A_DATA/UBIQUM/TASK3.3_WIFI/task3-2-wifi-PCANALS")
+
+# wifi_train_o<-read.csv(file = "trainingData.csv", header = TRUE, sep =",")
+# save(wifi_train_o, file = "wifi_train_o.Rdata")
+load(file = "wifi_train_o.Rdata")
+
+# wifi_validation_o<-read.csv(file = "validationData.csv")
+# save(wifi_validation_o, file = "wifi_validation_o.Rdata")
+load(file = "wifi_validation_o.Rdata")
+
+
+wifi_train<-wifi_train_o # to keep original#
+
+
+
+
+#### B._ SUBSETS####
+
+#identify columns with only 100 values###
+
+
+wifi_train2<-wifi_train[,1:520]!=100
+
+####subset by building####
+wifi_t_b0<-wifi_train%>%dplyr:::filter(BUILDINGID == 0) #floors 0 to 3
+wifi_t_b1<-wifi_train%>%dplyr:::filter(BUILDINGID == 1) #floors 0 to 3
+wifi_t_b2<-wifi_train%>%dplyr:::filter(BUILDINGID == 2) #floors 0 to 4 
+
+
+
+                                                                                          q
+ggplot(wifi_t_b1, aes(x=FLOOR),)+ 
+         geom_jitter(aes(y=WAP400), na.rm = FALSE)
+
+####~~~~~~~~   NOTES - NEXT STEPS    ~~~~~~~~####
+#load(file = "wifi_validation_o.Rdata") #is not saving/loading#
