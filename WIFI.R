1f41895b142a1775ae5cfa4e482817301c983e5f
Pilar <pilarcanalsdros@gmail.com>
2018-12-10 18:05:54 +0100

    Max colums, removing rows without observations

--- WIFI.R
+++ WIFI.R
@@ -30,7 +30,10 @@ wifi_train<-wifi_train_o # to keep original#
 #### B._ SUBSETS####
 
 
-##changing values +100 to -110#
+###changing values +100 to -110###
+# when we find values= to 100 means 
+# not signal detection. In order to organize properly the bad and good signals 
+# we modify this values with a value = to not signal
 
 wifi_train[wifi_train==100]<--110
 
@@ -45,6 +48,60 @@ wifi_t_b2<-wifi_train%>%dplyr:::filter(BUILDINGID == 2) #floors 0 to 4
 
 waps<-grep("WAP", names(wifi_train), value = TRUE)  #WAP columns
 
+wifi_train$BUILDINGID<-as.factor(wifi_train$BUILDINGID)
+wifi_train$FLOOR<-as.factor(wifi_train$FLOOR)
+
+
+wifi_names<-names(wifi_train)
+
+waps<-grep("WAP", names(wifi_train), value = TRUE) 
+
+wifi_pos<-setdiff(wifi_names, waps)
+
+#remove rows without signal#
+wifi_train<-wifi_train%>%filter(apply(wifi_train[waps],1, function(x) length(unique(x))) > 1)
+
+##split df in with signal and without signal##
+
+#wifi_t_ns<-wifi_train[!vapply(wifi_train[waps], function(x) length(unique(x)) > 1, logical(1L))]
+#wifi_t_ns<-wifi_train[!apply(wifi_train[waps],2, function(x) length(unique(x))) > 1]
+wifi_t_ns<-wifi_train[!apply(wifi_train,2, function(x) length(unique(x))) > 1] #removes the waps with no signal#
+wifi_t_ws<-wifi_train[apply(wifi_train,2, function(x) length(unique(x))) > 1]  #keeps waps with signal#
+
+waps_ws<-grep("WAP", names(wifi_t_ws), value = TRUE)
+
+#colum with max signal#
+
+wifi_t_ws_max<- apply(wifi_t_ws[waps_ws], 1, function(x) names(which.max(x)))
+wifi_t_ws$wifi_t_ws_max<-wifi_t_ws_max #new colum with max values name colums
+
+
+
+# d<-wifi_train[!(wifi_train[waps]=="-110"),]
+# wifi_t_signal<-wifi_train[apply(wifi_train[waps],2, function(x) min(x) ==-110)] #con señal#
+# wifi_t_nosignal<-wifi_train[apply(wifi_train,2, function(x) max(x) ==-110)]
+#wifi_t_signal<-wifi_train[!apply(wifi_train[waps],2, function(x) min(unique(x))) > 1]
+#df with all attributes except waps# NOT WORKING#
+# wifi_nowaps_df <- setdiff(wifi_train,wifi_pos)
+
+
+#ROW#
+
+# x<-wifi_train[apply(wifi_train[,waps],1,function(x)(min(x==-110)))]
+# > View(x)
+# > x<-wifi_train[!apply(wifi_train[,waps],1,function(x)(min(x==-110)))]
+# Error in `[.data.frame`(wifi_train, !apply(wifi_train[, waps], 1, function(x) (min(x ==  : 
+#  undefined columns selected
+# x<-wifi_train[!apply(wifi_train[,waps],1,function(x)(min(x <-110)))]
+# x<-wifi_train[!apply(wifi_train[waps],1,function(x)(min(x <-110)))]
+# x<-wifi_train[!apply(wifi_train[waps],1,function(x)(min(x=<-110)))]
+# Error: unexpected assignment in "x<-wifi_train[!apply(wifi_train[waps],1,function(x)(min(x=<-"
+# x<-wifi_train[!apply(wifi_train[1:520],1,function(x)(min==-110)))]
+# Error: unexpected ')' in "x<-wifi_train[!apply(wifi_train[1:520],1,function(x)(min==-110)))"
+
+
+##by building##
+
 
 
 wifi_t_b1_ns<-wifi_t_b1[!vapply(wifi_t_b1[waps], function(x) length(unique(x)) > 1, logical(1L))] #df waps wo signal
@@ -54,9 +111,7 @@ waps_t_b1_ns<-grep("WAP", names(wifi_t_b1_ns), value = TRUE) #WAP columns wo sig
 wifi_t_b1_ws<-wifi_t_b1%>%
   select(-waps_t_b1_ns) #df waps w signal
 
-#lo mismo por row#
-
-
+#### PLOTS ####
 theme_set(theme_light())
 plot(wifi_train$LATITUDE, wifi_train$LONGITUDE)#
      
@@ -65,20 +120,12 @@ qplot(x = LATITUDE, y = LONGITUDE, data = wifi_train)
 
 qplot(x = BUILDINGID, y = TIMESTAMP, data =wifi_train)
 
-ggplot(wifi_t_b1_ws) + geom_jitter(x=waps, )
-
-#ggplot(wifi_train, aes(LONGITUDE, LATITUDE)) +
-  geom_polygon()
-
-#plotly(username = wifi_t_b1_ws,key = FLOOR)
-
-#scatterD3(x = LATITUDE, y = LONGITUDE,)
-####
 
 ggplot(wifi_t_b1, aes(x=FLOOR),)+ 
-         geom_jitter(aes(y=WAP400), na.rm = FALSE)
+         geom_count(aes(y=WAP400), na.rm = FALSE)
 
 
 #tab<-tableplot(wifi_t_b1, plot = FALSE)
 
 ####~~~~~~~~   NOTES - NEXT STEPS    ~~~~~~~~####
+
