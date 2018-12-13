5a19365ba97d5d5015a87e8209b843a8869f1004
Pilar <pilarcanalsdros@gmail.com>
2018-12-13 17:37:38 +0100

    Floor predictions

--- WIFI.R
+++ WIFI.R
@@ -10,6 +10,7 @@ library(dplyr)
 library(ggplot2)
 library(caret)
 library(doParallel)
+library(randomForest)
 
 #### A._ SETTING FILES####
 
@@ -28,6 +29,12 @@ wifi_validation<-wifi_validation_o # to keep original#
 
 rm(wifi_train_o, wifi_validation_o)
 
+####NEW ATTRIBUTES####
+
+wifi_train$BF<-paste0("B", wifi_train$BUILDINGID, "F", wifi_train$FLOOR)
+wifi_validation$BF<-paste0("B", wifi_validation$BUILDINGID, "F", wifi_validation$FLOOR)
+
+
 #### TYPES ATTRIBUTES ###
 
 wifi_names<-names(wifi_train)
@@ -37,7 +44,7 @@ all.equal(wifi_names, wifi_names_v) #columns in validation and in train are the 
 #splitting in groups the columns names#
 waps<-grep("WAP", names(wifi_train), value = TRUE)
 nowaps<-setdiff(wifi_names, waps)
-fac<-c("FLOOR", "BUILDINGID", "SPACEID","RELATIVEPOSITION", "USERID", "PHONEID")
+fac<-c("FLOOR", "BUILDINGID", "SPACEID","RELATIVEPOSITION", "USERID", "PHONEID", "BF")
 
 wifi_train[fac] <- lapply(wifi_train[fac], as.factor) 
 wifi_validation[fac] <- lapply(wifi_validation[fac], as.factor) 
@@ -70,7 +77,6 @@ wifi_train<-wifi_train%>%filter(apply(wifi_train[waps],1, function(x) length(uni
 
 ##split df in with signal and without signal##
 
-
 df_nowaps<-wifi_train[nowaps]
 df_waps<-wifi_train[waps]
 df_nowaps_val<-wifi_validation[nowaps]
@@ -88,23 +94,47 @@ wifi_t_signal_val_names<-names(wifi_t_signal_val)
 
 rm(df_nowaps, df_waps)
 
-####intersect with validation###
+####intersect with validation####
+
 wifi_t_signal_intersect<-intersect(wifi_t_signal_names, wifi_t_signal_val_names)
 
 wifi_t_signal<-wifi_t_signal[,wifi_t_signal_intersect]
+wifi_t_signal_val<-wifi_t_signal_val[,wifi_t_signal_intersect]
 
 rm(wifi_t_signal_intersect, wifi_t_signal_names, wifi_t_signal_val_names)
-
-
-#####colum with max signal####
 waps_ws<-grep("WAP", names(wifi_t_signal), value = TRUE)
 
+
+####Remove vaues with les more than -30####
+
+x<-wifi_t_signal%>%filter(apply(wifi_t_signal[waps_ws],1, function(x) any(x>= -0.30)))
+y<-wifi_t_signal%>%filter(apply(wifi_t_signal[waps_ws],1, function(x) any(x< -0.80)))
+summary(y[nowaps])#vemos el user a investigar#
+
+wifi_t_signal%>%filter(USERID==14)%>%count()
+
+
+barplot(table(wifi_train$USERID))
+
+###column BF####
+
+#####colums with max signal MAX WAP & MAX SIGNAL####
+waps_ws<-grep("WAP", names(wifi_t_signal), value = TRUE)
+
+
 WAP_max<- apply(wifi_t_signal[waps_ws], 1, function(x) names(which.max(x)))
+WAP_max_value<-apply(wifi_t_signal[waps_ws], 1, function(x) max(x))
+
 wifi_t_signal$WAP_max<-WAP_max #new colum with max values name colums
+wifi_t_signal$WAP_max_value<-WAP_max_value
 
 
+WAP_max_val<- apply(wifi_t_signal_val[waps_ws], 1, function(x) names(which.max(x)))
+WAP_max_value_val<-apply(wifi_t_signal_val[waps_ws], 1, function(x) max(x))
 
-####B_removing more -30####
+wifi_t_signal_val$WAP_max<-WAP_max_val #new colum with max values name colums
+wifi_t_signal_val$WAP_max_value<-WAP_max_value_val
+
 
 
 
@@ -145,18 +175,19 @@ ctrl<-trainControl(method="repeatedcv", number = 10, repeats = 3, allowParallel 
 
 #### KNN with max####
 
-knn<-train(BUILDINGID~WAP_max,data=train,
-           method= "knn", trControl= ctrl,
-           preProcess= c("center","scale"),
-           tuneLength = 10)
+# knn_B<-train(BUILDINGID~WAP_max_value,data=wifi_t_signal,
+#            method= "knn", trControl= ctrl,
+#            tuneLength = 10)
+# 
+# knn_B
 
-knn
+#saveRDS(knn_B, file = "knn_B.rds")
+knn_B<-readRDS("knn_B.rds")
 
-#saveRDS(knn, file = "knn.rds")
-# knn<-readRDS("knn.rds")
+knn_BF<-readRDS("knn_BF.rds")
 
-pred<-predict(knn, test) ###error##
-confusionMatrix(pred, test$BUILDINGID)
+knn_pred_B<-predict(knn_B, wifi_t_signal_val)
+confusionMatrix(knn_pred_B, wifi_t_signal_val$BUILDINGID)
 
 #### SVM_ LINEAR with max#### 
 
@@ -165,28 +196,55 @@ confusionMatrix(pred, test$BUILDINGID)
 #                  kernel="radial", 
 #                  ranges=list(cost=10^(-2:2), gamma=2^(-2:2)))
 
-svm_l<-train(BUILDINGID~WAP_max,data=wifi_t_signal, #aplicado en el train completo#
+svm_l_lat<-train(LATITUDE~WAP_max,data=train, #aplicado en el train completo#
            method= "svmLinear", trControl= ctrl,
            tuneLength = 20)
-svm_l
+svm_l_lat
 
+#saveRDS(svm_l_f, file = "svm_l_f.rds")
+svm_l_f<-readRDS("svm_l_f.rds") #by floor# 0,90
+
+#saveRDS(svm_l, file = "svm_l.rds")
+svm_l<-readRDS("svm_l.rds") #by building# 0,99
+
+saveRDS(svm_l_lat, file="svm_l_lat.rds")
+svm_l_lat<-readRDS("svm_l_lat.rds") #by latitude# RMSE 65.
+
+
+pred_svm_l_B<-predict(svm_l, wifi_t_signal_val)
+confusionMatrix(pred_svm_l, wifi_t_signal_val$BUILDINGID)
+
+pred_svm_l_f<-predict(svm_l_f, wifi_t_signal_val)
+confusionMatrix(pred_svm_l_f, wifi_t_signal_val$BF)
+
+pred_svm_l_lat<-predict(svm_l_lat, wifi_t_signal_val)
+confusionMatrix(pred_svm_l_lat, wifi_t_signal_val$LATITUDE)
 
 #### SVM_ Radial with max####
 
-svm_r<-train(BUILDINGID~WAP_max,data=train,
-             method= "svmRadial", trControl= ctrl,
-             tuneLength = 20)
-svm_r
+# svm_r<-train(BUILDINGID~WAP_max,data=train,
+#              method= "svmRadial", trControl= ctrl,
+#              tuneLength = 20)
+# svm_r
 
 #### SVM_ Polynominal with max####
 
-svm_p<-train(BUILDINGID~WAP_max,data=train,
-             method= "svmPoly", trControl= ctrl,
-             tuneLength = 10, )
-svm_p
+# svm_p<-train(BUILDINGID~WAP_max,data=train,
+#              method= "svmPoly", trControl= ctrl,
+#              tuneLength = 10)
+# svm_p
 
 
 #### Random Forest with max####
+
+
+system.time(rf_BF <- randomForest(BUILDINGID~WAP_max, 
+                                  data = train, 
+                                  method="rf", trControl= ctrl,
+                                  ntree=10,
+                                  allowParalel=TRUE))
+
+
 
 # rf<-train(BUILDINGID~WAP_max,data=train,
 #           method= "rf", trControl= ctrl,
@@ -200,6 +258,7 @@ svm_p
 
 
 
+stopCluster(cl)
 
 
 
@@ -228,9 +287,20 @@ wifi_t_b1_ws<-wifi_t_b1%>%
 # qplot(x = BUILDINGID, y = TIMESTAMP, data =wifi_train)
 # 
 # 
-# ggplot(wifi_t_b1, aes(x=FLOOR),)+ 
-#          geom_count(aes(y=WAP400), na.rm = FALSE)
+ggplot(wifi_t_signal, aes(x=BUILDINGID),)+
+         geom_count(aes(y=WAP323), na.rm = FALSE)
 
+ggplot(wifi_t_signal_val, aes(x=BUILDINGID),)+
+  geom_count(aes(y=WAP323), na.rm = FALSE)
+
+ggplot(wifi_t_signal, aes(x=BF),)+
+  geom_count(aes(y=WAP508), na.rm = FALSE)
+
+ggplot(wifi_t_signal_val, aes(x=BF),)+
+  geom_count(aes(y=WAP508), na.rm = FALSE)
+
+
+#268, 323
 
 #tab<-tableplot(wifi_t_b1, plot = FALSE)
 
